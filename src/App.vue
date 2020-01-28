21980514a4268ed14d1c1fa3bde296a4970811af
Kodziak <kodziak1416@gmail.com>
2020-01-28 20:13:15 +0100

    Add fade transition on component change.

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,9 @@
   <div id="app">
     <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
     <!-- <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/> -->
-    <router-view />
+    <transition name="component-fade" mode="out-in">
+      <router-view ></router-view>
+    </transition>
   </div>
 </template>
 
@@ -24,5 +26,12 @@ export default class App extends Vue {}
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.component-fade-enter-active, .component-fade-leave-active {
+  transition: opacity .2s ease;
+}
+.component-fade-enter, .component-fade-leave-to {
+  opacity: 0;
 }
 </style>
