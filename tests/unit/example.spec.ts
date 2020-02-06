b2030a1e9e3ec8155de06f2a3908270fc20a153a
Kodziak <kodziak1416@gmail.com>
2020-02-07 00:08:45 +0100

    Add unit test

--- tests/unit/example.spec.ts
+++ tests/unit/example.spec.ts
@@ -1,12 +1,12 @@
-// import { shallowMount } from '@vue/test-utils';
-// import HelloWorld from '@/components/HelloWorld.vue';
+import { shallowMount } from '@vue/test-utils';
+import RefButton from '@/components/buttons/RefButton.vue';
 
-// describe('HelloWorld.vue', () => {
-//   it('renders props.msg when passed', () => {
-//     const msg = 'new message';
-//     const wrapper = shallowMount(HelloWorld, {
-//       propsData: { msg },
-//     });
-//     expect(wrapper.text()).toMatch(msg);
-//   });
-// });
+describe('RefButton.vue', () => {
+  it('renders props.title when passed', () => {
+    const title = 'Change Password';
+    const wrapper = shallowMount(RefButton, {
+      propsData: { title },
+    });
+    expect(wrapper.text()).toMatch(title);
+  });
+});
