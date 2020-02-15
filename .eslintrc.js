68aa2fb7823f716de0f9da5496a6523aff8e6a26
Kodziak <kodziak1416@gmail.com>
2020-02-15 21:01:17 +0100

    Change command

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,7 +8,7 @@ module.exports = {
     '@vue/airbnb',
     '@vue/typescript',
     'plugin:vue/recommended',
-    'plugin:vue-scoped-css/recommended'
+    'plugin:vue-scoped-css/recommended',
   ],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
